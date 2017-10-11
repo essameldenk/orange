3fda9d28727e8313a0de1699a5f095ac739f60d1
essam <essameldenk>
2017-10-11 11:08:12 +0200

    second_python

---
+++ try2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from datetime import datetime
+from datetime import timedelta
+files=['Gi-aggregated-report-working_2017.10.04 at 08_0223 EET','Gi-aggregated-report-working_2017.10.03 at 08_0220 EET','Gi-aggregated-report-working_2017.10.02 at 08_0251 EET']
+
+x1=files[0]
+ 
+x2=files[1]
+
+x3=files[2]
+
+#print x1,'\n',x2,'\n',x3
+
+print x1[29:39]	
+
+
